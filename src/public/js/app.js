11a62f4acd68f6999ac643439d564cb4cd6193e8
JYPIT <jypit@naver.com>
2022-08-18 22:18:20 +0900

    nickname-message form 구성

---
+++ src/public/js/app.js
@@ -0,0 +1,39 @@
+const messageList = document.querySelector("ul");
+const nickForm = document.querySelector("#nick");
+const messageForm = document.querySelector("#message");
+const frontSocket = new WebSocket(`ws://${window.location.host}`);
+
+function makeMessage(type, payload) {
+  const msg = { type, payload };
+  return JSON.stringify(msg);
+}
+
+frontSocket.addEventListener("open", () => {
+  console.log("Connected to Server 🔥");
+});
+
+frontSocket.addEventListener("message", (message) => {
+  const li = document.createElement("li");
+  li.innerText = message.data;
+  messageList.append(li);
+});
+
+frontSocket.addEventListener("close", () => {
+  console.log("Diconnected to Server 💧");
+});
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const input = messageForm.querySelector("input");
+  frontSocket.send(makeMessage("new_message", input.value));
+  input.value = "";
+}
+
+function handleNickSubmit(event) {
+  event.prventDefault();
+  const input = nickForm.querySelector("input");
+  frontSocket.send(makeMessage("nickname", input.value));
+}
+
+messageForm.addEventListener("submit", handleSubmit);
+nickForm.addEventListener("submit", handleNickSubmit);
--- src/server.js
+++ src/server.js
@@ -1,7 +1,36 @@
+import http from "http";
+import WebSocket from "ws";
 import express from "express";
+import { Socket } from "dgram";
+import { SocketAddress } from "net";
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname + "/views");
+app.use("/public", express.static(__dirname + "/public"));
+app.get("/", (_, res) => res.render("home"));
+app.get("/*", (req, res) => res.redirect("/"));
 
-app.listen(3000);
+const handleListen = () => console.log(`🚀 Listening on http://localhost:3000 🚀`);
+
+const server = http.createServer(app);
+const wss = new WebSocket.Server({ server });
+
+const onSocketClose = () => console.log("💧 Disconnected from the Browser 💧");
+
+const backSockets = [];
+
+wss.on("connection", (backSocket) => {
+  backSockets.push(backSocket);
+  console.log("🔥 Connected to Browser 🔥");
+  backSocket.on("close", onSocketClose);
+  backSocket.on("message", (message) => {
+    const parsed = JSON.parse(message);
+    if (parsed.type === "new_message") {
+      backSockets.forEach((aSocket) => aSocket.send(parsed.payload));
+    }
+  });
+});
+
+server.listen(3000, handleListen);
